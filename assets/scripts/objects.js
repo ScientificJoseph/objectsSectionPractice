b07aa215c308d3b6e37ada92e899084ac501732e
TekniqueJoseph <tekniquejoseph@gmail.com>
2023-06-10 12:16:39 -0400

    worked with square bracket notation

--- assets/scripts/objects.js
+++ assets/scripts/objects.js
@@ -1,13 +1,19 @@
 const movieList = document.getElementById('movie-list');
 movieList.style['background-color'] = 'red';
 movieList.style.display = 'block'
+
+const userKeyName = 'level';
+// const userKeyName = prompt('Enter the keyname')
+
 let person = {
     'first name': 'Joseph',
     age: 50,
     hobbies: ['Music', 'Coding'],
+    [userKeyName]: 5,
     greet: function() {
         alert('Hey you!')
-    }
+    },
+    1.5: 'hello'
 };
 
 // person.age = 51;
@@ -16,6 +22,12 @@ delete person.age;
 // person.age = null
 person.isAdmin = true;
 
+const keyName = 'first name'
+console.log(`${keyName}: ${person[keyName]}`)
 
-console.log(person['first name']);
+// console.log(person['first name']);
+console.log(person[keyName]) //with quotes means you looking for a property in the objext with keyName as the key/(name). witout means ur looking for the value of the the const keyName to be a property in the person object. the above notation would yield that properties value. 
+console.log(person[1.5]);
+console.log(person)
+console.log(person['userKeyName'])
 
